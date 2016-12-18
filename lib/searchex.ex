52684573f752dcc792f60b61155ceb964e93fac7
andyl <andy@r210.com>
2016-12-18 10:06:13 -0800

    Add new Searchex module

---
+++ lib/searchex.ex
@@ -0,0 +1,41 @@
+defmodule Searchex do
+
+  @moduledoc "App settings and metadata"
+
+  @doc "Return the application version"
+  def version do
+    {:ok, "#{Util.Ext.App.version}"}
+  end
+
+  @core_base_path "~/.searchex"
+  @test_base_path "test/data"
+
+  @doc """
+  Return the base path
+
+  In production, the base path is typically `~/.searchex`.
+
+  The directory structure organization:
+
+      base_dir > repo_dirs > collection_files
+
+  There is one base dir.  Underneath the base dir there are multiple
+  repo dirs.  In each repo dir, there can be multiple collection files.
+  Collection files are yaml files with collection configuration parameters.
+
+  TODO: read the base path from ~/.searchexrc (TOML format)
+  """
+  def base_path do
+    case Mix.env do
+      :test -> expand(@test_base_path)
+      :eval -> expand(@core_base_path)
+      _     -> expand(@core_base_path)
+    end
+  end
+
+  # -----
+
+  defp expand(path) do
+    Path.expand(path)
+  end
+end
