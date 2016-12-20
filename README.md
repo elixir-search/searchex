e8c925e72fc035d3f997525d6377117d9112a7f9
andyl <andy@r210.com>
2016-12-20 13:20:25 -0800

    Update README with QuickStart info

--- README.md
+++ README.md
@@ -24,10 +24,6 @@ document types include:
 - chat rooms and twitter feeds
 - web pages
 
-## Quick Start
-
-TBD
-
 ## Searchex Architecture
 
 A Searchex `DOCUMENT` has two key elements:
@@ -73,13 +69,27 @@ Add `searchex` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:searchex, "~> 0.0.1-alpha.2"}]
+  [{:searchex, "~> 0.0.1-alpha.3"}]
 end
 ```
-
 Then run `mix deps.get`
 
 View API documentation at https://hexdocs.pm/searchex
+
+
+## Quick Start
+
+After the `searchex` escript is installed...
+
+1) Fetch a Searchex repository
+ 
+    > searchex fetch elixir-search/sample
+
+2) Run
+
+    > searchex help                         # show help page
+    > searchex ls                           # list collections
+    > searchex query <collection> 'query'   # run a query
 
 ## Roadmap
 
--- lib/searchex/cli.ex
+++ lib/searchex/cli.ex
@@ -16,26 +16,26 @@ defmodule Searchex.Cli do
   # name.  Argument and Description are used to generate help text.
   cmd_opts = [
     # Cmd      Arity    Module       Argument                 Description
-    {"ls"      ,   0,   "Config"   , ""                     , "list collections"                },
-    {"new"     ,   1,   "Config"   , "TARGET_PATH"          , "new collection for TARGET_PATH"  },
-    {"cat"     ,   1,   "Config"   , "COLLECTION"           , "cat config"                      },
-    {"modify"  ,   1,   "Render"   , "COLLECTION"           , "edit the config file"            },
-    {"rm"      ,   1,   "Config"   , "COLLECTION"           , "remove config"                   },
-    {"build"   ,   1,   "Render"   , "COLLECTION"           , "build the collection"            },
-    {"query"   ,   2,   "Render"   , "COLLECTION '<query>'" , "search the collection"           },
-    {"results" ,   1,   "Render"   , "COLLECTION"           , "results from the last query"     },
-    {"show"    ,   2,   "Render"   , "COLLECTION DOCID"     , "show text of DOCID"              },
-    {"edit"    ,   2,   "Render"   , "COLLECTION DOCID"     , "edit DOCID"                      },
-    {"info"    ,   1,   "Render"   , "COLLECTION"           , "status and statistics"           },
-    {"version" ,   0,   ""         , ""                     , "show installed version"          },
-    {"help"    ,   0,   "Cli"      , ""                     , "this command"                    },
+    {"ls"      ,   0,   "Config"   , ""                     , "list collections"                       },
+    {"new"     ,   1,   "Config"   , "TARGET_PATH"          , "new collection for TARGET_PATH"         },
+    {"cat"     ,   1,   "Config"   , "COLLECTION"           , "cat config"                             },
+    {"modify"  ,   1,   "Render"   , "COLLECTION"           , "edit the config file"                   },
+    {"rm"      ,   1,   "Config"   , "COLLECTION"           , "remove config"                          },
+    {"build"   ,   1,   "Render"   , "COLLECTION"           , "build the collection"                   },
+    {"query"   ,   2,   "Render"   , "COLLECTION '<query>'" , "search the collection"                  },
+    {"results" ,   1,   "Render"   , "COLLECTION"           , "results from the last query"            },
+    {"show"    ,   2,   "Render"   , "COLLECTION DOCID"     , "show text of DOCID"                     },
+    {"edit"    ,   2,   "Render"   , "COLLECTION DOCID"     , "edit DOCID"                             },
+    {"info"    ,   1,   "Render"   , "COLLECTION"           , "status and statistics"                  },
+    {"fetch"   ,   1,   "Config"   , "REPO_URL"             , "fetch repo (eg 'elixir-search/sample')" },
+    {"version" ,   0,   ""         , ""                     , "show installed version"                 },
+    {"help"    ,   0,   "Cli"      , ""                     , "this command"                           },
   ]
   @cmd_opts cmd_opts
 
   # These command options are not included in the CLI 'help' output.
   alt_opts = [
     # Cmd          Arity    Module       Argument             Description
-    {"fetch"       ,   1,   "Config"   , "URL"              , "fetch Searchex repo"                      },
     {"catalog"     ,   1,   "Render"   , "COLLECTION"       , "build the collection catalog"             },
     {"index"       ,   1,   "Render"   , "COLLECTION"       , "build the collection index"               },
     {"clean"       ,   1,   "Command"  , "COLLECTION"       , "remove cache for COLLECTION"              },
