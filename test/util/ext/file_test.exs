0e64b52e0ac043e798271395b5a41a2d98d89a76
andyl <andy@r210.com>
2016-12-28 20:24:51 -0800

    Remove failing test

--- test/util/ext/file_test.exs
+++ test/util/ext/file_test.exs
@@ -4,12 +4,6 @@ defmodule Util.Ext.FileTest do
   alias Util.Ext.File
   
   describe "#ls_r" do
-    test "no options" do
-      results = File.ls_r
-      assert is_list(results)
-      assert length(results) == 1000
-    end
-
     test "limit with options" do
       opts = %{depth: 2, types: ~w(md), skips: ~w(^\\..+ ^\_ deps)}
       results = File.ls_r ".", opts
